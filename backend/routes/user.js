84771f4531cf797353475b8745550d363db1055b
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-20 20:11:53 +0200

    Refactor authentication logic into user controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,49 +1,8 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
 const router = express.Router();
+const userCtrl = require('../controllers/user'); // Importation du contrôleur
 
-// Route d'inscription (signup)
-router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-});
-
-// Route de connexion (login)
-router.post('/login', (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
-          const token = jwt.sign(
-            { userId: user._id },
-            'RANDOM_TOKEN_SECRET', 
-            { expiresIn: '24h' }
-          );
-          res.status(200).json({
-            userId: user._id,
-            token: token
-          });
-        })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-});
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
