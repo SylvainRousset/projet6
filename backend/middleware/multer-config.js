00debf6c203efc3f9bdfc7575328314b9bfb7f86
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-21 17:05:28 +0200

    Add Sharp for image resizing and optimization

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,32 +1,37 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
-const dotenv = require('dotenv');
+const cloudinary = require('./cloudinaryConfig');
+const sharp = require('sharp');
 
-dotenv.config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'books', // Le dossier où les images seront stockées sur Cloudinary
-    allowed_formats: ['jpg', 'png', 'webp'], // Formats autorisés
-  },
-});
-
+const storage = multer.memoryStorage();  // mémoire tampon
 const upload = multer({ storage: storage }).single('imageUrl');
 
 const multerMiddleware = (req, res, next) => {
-  upload(req, res, function (err) {
+  upload(req, res, async function (err) {
     if (err) {
       return res.status(400).json({ error: err.message });
     }
-    next();
+
+    const buffer = req.file.buffer; //  buffer en mémoire
+
+    try {
+      const resizedBuffer = await sharp(buffer)
+        .resize(206, 260)
+        .toFormat('webp')
+        .toBuffer();  
+
+      // Upload  Cloudinary 
+      const result = await cloudinary.uploader.upload_stream({ folder: 'books' }, (error, result) => {
+        if (error) {
+          return res.status(500).json({ error: error.message });
+        }
+
+        req.file.path = result.secure_url;
+        next(); 
+      }).end(resizedBuffer); // Transmettre Cloudinary
+
+    } catch (error) {    
+      return res.status(500).json({ error: error.message });
+    }
   });
 };
 
