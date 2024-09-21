24a3a4c5ca455286d684422e46acf79728092e84
SylvainRousset <sylvaindebisca@hotmail.fr>
2024-09-21 14:33:15 +0200

    Updated authentication

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,12 +8,13 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
+    
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'User ID non valable !';
+      throw new Error('User ID non valable !');
     } else {
       next();
     }
   } catch (error) {
-    res.status(401).json({ error: 'Requête non authentifiée !' });
+    res.status(401).json({ error: error.message });  
   }
 };
